d7a75cf2f9cbb3f883e8e3f3d7b7f172435eb7a0
なるみ <weaper@gmail.com>
2019-06-01 23:48:49 +0800

    Add efficientnet.py

---
+++ efficientnet.py
@@ -0,0 +1,137 @@
+from torch import nn
+
+
+class Swish(nn.Module):
+
+    def __init__(self, *args, **kwargs):
+        super(Swish, self).__init__()
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
+
+
+class ConvBNReLU(nn.Sequential):
+
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, groups=1):
+        padding = (kernel_size - 1) // 2
+        super(ConvBNReLU, self).__init__(
+            nn.Conv2d(in_planes, out_planes, kernel_size, stride, padding, groups=groups, bias=False),
+            nn.BatchNorm2d(out_planes),
+            Swish(),
+        )
+
+
+class SqueezeExcitation(nn.Module):
+
+    def __init__(self, num_features, reduction_ratio=4):
+        super(SqueezeExcitation, self).__init__()
+        hidden_dim = int(num_features / reduction_ratio)
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(num_features, hidden_dim, 1, bias=True),
+            Swish(),
+            nn.Conv2d(hidden_dim, num_features, 1, bias=True),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        return x * self.se(x)
+
+
+class MBConvBlock(nn.Module):
+
+    def __init__(self, in_planes, out_planes, expand_ratio, kernel_size, stride):
+        super(MBConvBlock, self).__init__()
+        self.use_residual = in_planes == out_planes and stride == 1
+        assert stride in [1, 2]
+        assert kernel_size in [3, 5]
+
+        hidden_dim = int(in_planes * expand_ratio)
+        layers = []
+
+        # pw
+        if in_planes != hidden_dim:
+            layers += [ConvBNReLU(in_planes, hidden_dim, 1)]
+
+        layers += [
+            # dw
+            ConvBNReLU(hidden_dim, hidden_dim, kernel_size, stride=stride, groups=hidden_dim),
+            # se
+            SqueezeExcitation(hidden_dim),
+            # pw-linear
+            nn.Conv2d(hidden_dim, out_planes, 1, bias=False),
+            nn.BatchNorm2d(out_planes),
+        ]
+
+        self.conv = nn.Sequential(*layers)
+
+    def forward(self, x):
+        if self.use_residual:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+
+class EfficientNet(nn.Module):
+    """
+    https://github.com/tensorflow/tpu/tree/master/models/official/efficientnet
+    """
+
+    def __init__(self, num_classes=1000, width_mult=1.0, depth=1.0, resolution=1.0):
+        super(EfficientNet, self).__init__()
+
+        # yapf: disable
+        settings = [
+            # t,  c, n, s, k
+            [1,  16, 1, 1, 3],  # MBConv1_3x3, SE
+            [6,  24, 2, 2, 3],  # MBConv6_3x3, SE
+            [6,  40, 2, 2, 5],  # MBConv6_5x5, SE
+            [6,  80, 3, 2, 3],  # MBConv6_3x3, SE
+            [6, 112, 3, 1, 5],  # MBConv6_5x5, SE
+            [6, 192, 4, 2, 5],  # MBConv6_5x5, SE
+            [6, 320, 1, 1, 3]   # MBConv6_3x3, SE
+        ]
+        # yapf: enable
+
+        features = [ConvBNReLU(3, int(32 * width_mult), 3, stride=2)]
+
+        in_channels = int(32 * width_mult)
+        for t, c, n, s, k in settings:
+            out_channels = int(c * width_mult)
+            for i in range(n):
+                stride = s if i == 0 else 1
+                features += [MBConvBlock(in_channels, out_channels, expand_ratio=t, stride=stride, kernel_size=k)]
+                in_channels = out_channels
+
+        features += [ConvBNReLU(in_channels, 1280, 1)]
+        self.features = nn.Sequential(*features)
+        self.avg_pool = nn.AvgPool2d(kernel_size=7)
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.2),
+            nn.Linear(1280, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avg_pool(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+
+def efficientnet_b0():
+    return EfficientNet()
+
+
+def main():
+    import torch
+    m = EfficientNet()
+    x = torch.randn(1, 3, 224, 224)
+    with torch.no_grad():
+        y = m(x)
+        print(y.size())
+
+
+if __name__ == "__main__":
+    main()
