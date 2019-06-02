70ff09685f86573dcebf66aa330b1941090412f7
なるみ <weaper@gmail.com>
2019-06-02 22:38:32 +0800

    Update __init__.py

--- efficientnet/models/__init__.py
+++ efficientnet/models/__init__.py
@@ -1,7 +1,8 @@
 import sys
 
 from ..utils import get_factory
-from .efficientnet import (efficientnet_b0, efficientnet_b1, efficientnet_b2, efficientnet_b3, efficientnet_b4,
+from .efficientnet import (EfficientNet, efficientnet_b0, efficientnet_b1,
+                           efficientnet_b2, efficientnet_b3, efficientnet_b4,
                            efficientnet_b5, efficientnet_b6, efficientnet_b7)
 
 ModelFactory = get_factory(sys.modules[__name__])
