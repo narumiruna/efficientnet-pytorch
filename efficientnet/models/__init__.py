b5415de5399e037c62fc2c5da4038ea642db1532
なるみ <weaper@gmail.com>
2019-06-02 16:57:26 +0800

    Move efficientnets

---
+++ efficientnet/models/__init__.py
@@ -0,0 +1,2 @@
+from .efficientnet import (efficientnet_b0, efficientnet_b1, efficientnet_b2, efficientnet_b3, efficientnet_b4,
+                           efficientnet_b5, efficientnet_b6, efficientnet_b7)
