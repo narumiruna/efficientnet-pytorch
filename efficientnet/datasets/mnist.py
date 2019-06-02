4ecc5a501622882d110cdf43fa2c25c8dc39fb61
なるみ <weaper@gmail.com>
2019-06-02 21:25:00 +0800

    Extract in_channels

--- efficientnet/datasets/mnist.py
+++ efficientnet/datasets/mnist.py
@@ -2,16 +2,6 @@ from torch.utils import data
 from torchvision import datasets, transforms
 
 
-class Expand(object):
-
-    def __init__(self, dim=3):
-        self.dim = dim
-
-    def __call__(self, img):
-        _, h, w = img.size()
-        return img.expand(self.dim, h, w)
-
-
 class MNISTDataloader(data.DataLoader):
 
     def __init__(self, root: str, image_size: int, train: bool, batch_size: int, shuffle: bool, **kwargs):
@@ -19,7 +9,6 @@ class MNISTDataloader(data.DataLoader):
             transforms.Resize(image_size),
             transforms.ToTensor(),
             transforms.Normalize((0.1307,), (0.3081,)),
-            Expand(),
         ])
 
         dataset = datasets.MNIST(
