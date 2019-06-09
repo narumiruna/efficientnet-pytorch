6f39770f9c81b26e41983891b87ceda263628818
なるみ <weaper@gmail.com>
2019-06-09 17:08:26 +0800

    Add __repr__ to config class

--- efficientnet/config.py
+++ efficientnet/config.py
@@ -3,9 +3,19 @@ from .utils import AttrDict, load_yaml
 
 class Config(AttrDict):
 
-    distributed: dict = {}
+    distributed = None
 
     @classmethod
     def from_yaml(cls, f):
         data = load_yaml(f)
         return cls(**data)
+
+    def __repr__(self):
+        format_string = self.__class__.__name__ + '('
+
+        for k, v in self.items():
+            format_string += f'\n\t{k}: {v}'
+
+        format_string += '\n)'
+
+        return format_string
