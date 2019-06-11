7e93a57c251836f81a4b6ceed3ffe5585458fbd3
なるみ <weaper@gmail.com>
2019-06-11 11:07:11 +0800

    Support topk accuracy (#13)

--- efficientnet/metrics/accuracy.py
+++ efficientnet/metrics/accuracy.py
@@ -5,19 +5,26 @@ import torch
 
 class Accuracy(object):
 
-    def __init__(self):
+    def __init__(self, top_k=1):
+        self.top_k = top_k
         self.correct = 0
         self.count = 0
 
-    def update(self, pred: torch.Tensor, true: torch.Tensor):
-        assert pred.size(0) == true.size(0)
+    def update(self, output: torch.Tensor, target: torch.Tensor):
+        assert output.size(0) == target.size(0)
 
-        self.correct += pred.data.eq(true.data).sum().item()
-        self.count += pred.size(0)
+        with torch.no_grad():
+            _, pred = output.topk(self.top_k, 1, True, True)
+            pred = pred.t()
+            correct = pred.eq(target.view(1, -1).expand_as(pred))
+            correct_k = correct[:self.top_k].view(-1).float().sum(0, keepdim=True).item()
+
+        self.correct += correct_k
+        self.count += output.size(0)
 
     @property
     def accuracy(self):
-        return self.correct / self.count
+        return 100 * self.correct / self.count
 
     def __str__(self):
-        return '{:.2f}%'.format(self.accuracy * 100)
+        return '{:.2f}%'.format(self.accuracy)
