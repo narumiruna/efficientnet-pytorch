a7e0a64492e20a195bf1120d20f5cb08b1f700ca
なるみ <weaper@gmail.com>
2019-07-04 19:36:37 -0700

    Evaluation (#22)

--- README.md
+++ README.md
@@ -17,18 +17,24 @@ https://arxiv.org/abs/1905.11946
 $ python train.py -c /path/to/config -r /path/to/dataset
 ```
 
+### Evaluate
+
+```shell
+$ python evaluate.py --arch efficientnet_b0 -r /path/to/dataset
+```
+
 ## Pretrained models
 
 | Model Name | Top-1 Accuracy |
 | ------ | ------ |
-| efficientnet-b0 | 75.74% |
-| efficientnet-b1 |  |
-| efficientnet-b2 |  |
-| efficientnet-b3 |  |
-| efficientnet-b4 |  |
-| efficientnet-b5 |  |
-| efficientnet-b6 |  |
-| efficientnet-b7 |  |
+| efficientnet_b0 | 75.74% |
+| efficientnet_b1 |  |
+| efficientnet_b2 |  |
+| efficientnet_b3 |  |
+| efficientnet_b4 |  |
+| efficientnet_b5 |  |
+| efficientnet_b6 |  |
+| efficientnet_b7 |  |
 
 ## References
 
--- efficientnet_test.py
+++ efficientnet_test.py
@@ -1,6 +1,6 @@
 import torch
 
-from efficientnet.models import EfficientNet
+from efficientnet.models.efficientnet import EfficientNet, params
 
 
 def numel(model):
@@ -25,17 +25,6 @@ def print_num_parameters(model):
 
 
 def main():
-    params = {
-        'efficientnet-b0': (1.0, 1.0, 224, 0.2),
-        'efficientnet-b1': (1.0, 1.1, 240, 0.2),
-        'efficientnet-b2': (1.1, 1.2, 260, 0.3),
-        'efficientnet-b3': (1.2, 1.4, 300, 0.3),
-        'efficientnet-b4': (1.4, 1.8, 380, 0.4),
-        'efficientnet-b5': (1.6, 2.2, 456, 0.4),
-        'efficientnet-b6': (1.8, 2.6, 528, 0.5),
-        'efficientnet-b7': (2.0, 3.1, 600, 0.5),
-    }
-
     for arch, (w, d, s, r) in params.items():
         model = EfficientNet(w, d, r)
 
