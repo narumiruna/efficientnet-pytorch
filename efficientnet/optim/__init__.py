ce8436bc5012d653ccd47c08f130acbc365da0eb
なるみ <weaper@gmail.com>
2019-08-12 19:44:59 +0800

    Fix import error

--- efficientnet/optim/__init__.py
+++ efficientnet/optim/__init__.py
@@ -4,7 +4,7 @@ from torch import optim
 from torch.optim import SGD
 
 from ..utils import get_factory
-from .rmsprop import RMSprop
+from .rmsprop import TFRMSprop
 
 OptimFactory = get_factory(sys.modules[__name__])
 SchedulerFactory = get_factory(optim.lr_scheduler)
