864e77af5a61ec596313d309a541878af516bfe2
なるみ <weaper@gmail.com>
2020-05-18 17:48:42 +0800

    Support torch hub (#31)
    * Support torch.hub
    
    * Update README.md
    
    * Update requirements.txt
    
    * Add isort config

---
+++ hubconf.py
@@ -0,0 +1,11 @@
+dependencies = ['torch', 'mlconfig', 'tqdm']
+
+
+from efficientnet.models.efficientnet import efficientnet_b0
+from efficientnet.models.efficientnet import efficientnet_b1
+from efficientnet.models.efficientnet import efficientnet_b2
+from efficientnet.models.efficientnet import efficientnet_b3
+from efficientnet.models.efficientnet import efficientnet_b4
+from efficientnet.models.efficientnet import efficientnet_b5
+from efficientnet.models.efficientnet import efficientnet_b6
+from efficientnet.models.efficientnet import efficientnet_b7
